171d8ccbd23439fadbcaeac71cf277721ac941dd
resabut <esfontjo@gmail.com>
2024-05-26 20:00:34 +0200

    adds commands to download file

--- scripts/x_joaes_read_QC.sh
+++ scripts/x_joaes_read_QC.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "script start: download and initial sequencing read quality control"
 date
-# ... some commands ...
+# download samples
+echo "Downloading samples"
+cat /proj/applied_bioinformatics/users/x_joaes/MedBioinfo/analyses/x_joaes_run_accessions.txt | srun --cpus-per-task=1 --time=00:30:00 singularity exec $MYIMAGE fastq-dump xargs --split-files --outdir /proj/applied_bioinformatics/users/x_joaes/MedBioinfo/data/sra_fastq/ --disable-multithreading --readids --gzip 
+echo "Done downloading samples"
+
 date
 echo "script end."
