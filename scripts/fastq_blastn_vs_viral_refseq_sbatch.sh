e530c4814194cba87c4747c943a716ec48975d77
resabut <esfontjo@gmail.com>
2024-05-27 16:55:59 +0200

    memory allocation down to 96g the most common

--- scripts/fastq_blastn_vs_viral_refseq_sbatch.sh
+++ scripts/fastq_blastn_vs_viral_refseq_sbatch.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1                   # nb of *tasks* to be run in // (usually 1), this task can be multithreaded (see cpus-per-task)
 #SBATCH --nodes=1                    # nb of nodes to reserve for each task (usually 1)
 #SBATCH --cpus-per-task=1            # nb of cpu (in fact cores) to reserve for each task /!\ job killed if commands below use more cores
-#SBATCH --mem=250GB                  # amount of RAM to reserve for the tasks /!\ job killed if commands below use more RAM
+#SBATCH --mem=96GB                  # amount of RAM to reserve for the tasks /!\ job killed if commands below use more RAM
 #SBATCH --time=0-02:00               # maximal wall clock duration (D-HH:MM) /!\ job killed if commands below take more time than reservation
 #SBATCH -o ./log/slurm.%A.%a.out   # standard output (STDOUT) redirected to these files (with Job ID and array ID in file names)
 #SBATCH -e ./log/slurm.%A.%a.err   # standard error  (STDERR) redirected to these files (with Job ID and array ID in file names)
--- scripts/fastq_subsamples_blastn_vs_viral_refseq_sbatch.sh
+++ scripts/fastq_subsamples_blastn_vs_viral_refseq_sbatch.sh
@@ -3,8 +3,8 @@
 #SBATCH --ntasks=1                   # nb of *tasks* to be run in // (usually 1), this task can be multithreaded (see cpus-per-task)
 #SBATCH --nodes=1                    # nb of nodes to reserve for each task (usually 1)
 #SBATCH --cpus-per-task=1            # nb of cpu (in fact cores) to reserve for each task /!\ job killed if commands below use more cores
-#SBATCH --mem=250GB                  # amount of RAM to reserve for the tasks /!\ job killed if commands below use more RAM
-#SBATCH --time=0-02:00               # maximal wall clock duration (D-HH:MM) /!\ job killed if commands below take more time than reservation
+#SBATCH --mem=96GB                  # amount of RAM to reserve for the tasks /!\ job killed if commands below use more RAM
+#SBATCH --time=0-00:30               # maximal wall clock duration (D-HH:MM) /!\ job killed if commands below take more time than reservation
 #SBATCH -o ./analyses/slurm.%A.%a.out   # standard output (STDOUT) redirected to these files (with Job ID and array ID in file names)
 #SBATCH -e ./analyses/slurm.%A.%a.err   # standard error  (STDERR) redirected to these files (with Job ID and array ID in file names)
 # /!\ Note that the ./outputs/ dir above needs to exist in the dir where script is submitted **prior** to submitting this script
